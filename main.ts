994f4fa72db6fd23b3eab9d4faea889329c54da8
ra106090 <112607026+ra106090@users.noreply.github.com>
2022-09-06 13:32:27 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,17 @@
+enum RadioMessage {
+    message1 = 49434
+}
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showString("" + (number))
+})
 input.onButtonPressed(Button.A, function () {
-    radio.sendString(":)")
+    number += 1
 })
-radio.onReceivedString(function (receivedString) {
-    basic.showString("RECEIVEDSCRING")
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendNumber(number)
 })
+input.onButtonPressed(Button.B, function () {
+    number = 0
+})
+let number = 0
 radio.setGroup(1)
